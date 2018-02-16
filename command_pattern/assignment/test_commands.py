6dff5a6ecf00cc0bcd44169682f2c9761a452505
Zach Lee <zach.lee@concur.com>
2018-02-16 15:29:35 -0800

    Create a general purpose test door and invoker

--- command_pattern/assignment/test_commands.py
+++ command_pattern/assignment/test_commands.py
@@ -1,13 +1,30 @@
+import pytest
+
 from actions.door import Door
 from commands import door_commands
 from commands import invoker
 
-def test_door():
-    door = Door('airlock')
-    open_command = door_commands.LockCommand(door)
-    close_command = door_commands.UnlockCommand(door)
+
+@pytest.fixture
+def door_and_invoker():
+    test_door = Door('test door')
+    open_command = door_commands.LockCommand(test_door)
+    close_command = door_commands.UnlockCommand(test_door)
     command_invoker = invoker.Invoker()
-    command_invoker.set_object_commands(door, (open_command, close_command))
-    command_invoker.activate(door)
+    command_invoker.set_object_commands(test_door, (open_command,
+                                                   close_command))
+    return test_door, command_invoker
+
+
+def test_basic_door_lock(door_and_invoker):
+    door, invoker = door_and_invoker
+    invoker.activate(door)
+    assert door.locked
+
+def test_basic_door_unlock(door_and_invoker):
+    door, invoker = door_and_invoker
+    door.unlock()
+    invoker.deactivate(door)
+    assert not door.locked
 
 
