f2afaaa8fb3007f3902deeeaabe6f99258c3937c
Zach Lee <zach.lee@concur.com>
2018-02-16 15:58:18 -0800

    Add more tests

--- command_pattern/assignment/test_commands.py
+++ command_pattern/assignment/test_commands.py
@@ -9,11 +9,11 @@ from commands import invoker
 @pytest.fixture
 def door_and_invoker():
     test_door = Door('test door')
-    open_command = door_commands.LockCommand(test_door)
-    close_command = door_commands.UnlockCommand(test_door)
+    activation_command = door_commands.LockCommand(test_door)
+    deactivation_command = door_commands.UnlockCommand(test_door)
     command_invoker = invoker.Invoker()
-    command_invoker.set_object_commands(test_door, (open_command,
-                                                   close_command))
+    command_invoker.set_object_commands(test_door, (activation_command,
+                                                   deactivation_command))
     return test_door, command_invoker
 
 
@@ -25,9 +25,23 @@ def test_basic_door_lock(door_and_invoker):
 
 def test_basic_door_unlock(door_and_invoker):
     door, invoker = door_and_invoker
-    door.unlock()
+    door.lock()
     invoker.deactivate(door)
     assert not door.locked
+
+def test_door_lock_and_unlock(door_and_invoker):
+    door, invoker = door_and_invoker
+    invoker.activate(door)
+    invoker.deactivate(door)
+    assert not door.locked
+
+def test_door_unlock_and_lock(door_and_invoker):
+    door, invoker = door_and_invoker
+    door.lock()
+    invoker.deactivate(door)
+    invoker.activate(door)
+    assert door.locked
+
 
 
 def test_null_command_if_unrecognized_object_activated():
@@ -37,4 +51,9 @@ def test_null_command_if_unrecognized_object_activated():
     l.check(('root', 'INFO', 'null command.'))
 
 
+def test_null_command_if_unrecognized_object_deactivated():
+    with testfixtures.LogCapture() as l:
+        command_invoker = invoker.Invoker()
+        command_invoker.deactivate('abc')
+    l.check(('root', 'INFO', 'null command.'))
 
